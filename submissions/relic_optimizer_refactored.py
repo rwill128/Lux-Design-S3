f3890d9995f966145fa615f0dc69a4dd6e2a74ab
Richard Williams <rwill128.create@gmail.com>
2024-12-18 21:59:24 -0300

    Some refactorings.

--- submissions/relic_optimizer_refactored.py
+++ submissions/relic_optimizer_refactored.py
@@ -1,10 +1,25 @@
-import os
-from luxai_s3.wrappers import LuxAIS3GymEnv, RecordEpisode
+import json
+from argparse import Namespace
 
-from agents.baseline_agent.baselineagent import BaselineAgent
-
-from scipy.optimize import linear_sum_assignment
 import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+def direction_to(src, target):
+    ds = target - src
+    dx = ds[0]
+    dy = ds[1]
+    if dx == 0 and dy == 0:
+        return 0
+    if abs(dx) > abs(dy):
+        if dx > 0:
+            return 2
+        else:
+            return 4
+    else:
+        if dy > 0:
+            return 3
+        else:
+            return 1
 
 class RelicHuntingShootingAgent:
     def __init__(self, player: str, env_cfg) -> None:
@@ -416,46 +431,60 @@ class RelicHuntingShootingAgent:
 
         return actions
 
+def from_json(state):
+    if isinstance(state, list):
+        return np.array(state)
+    elif isinstance(state, dict):
+        out = {}
+        for k in state:
+            out[k] = from_json(state[k])
+        return out
+    else:
+        return state
 
-def evaluate_agents(agent_1_cls, agent_2_cls, seed=42, games_to_play=3, replay_save_dir="replays"):
-    # Ensure the replay directory exists
-    os.makedirs(replay_save_dir, exist_ok=True)
+### DO NOT REMOVE THE FOLLOWING CODE ###
+agent_dict = dict() # store potentially multiple dictionaries as kaggle imports code directly
+agent_prev_obs = dict()
 
-    # Create an environment wrapped to record episodes
-    env = RecordEpisode(
-        LuxAIS3GymEnv(numpy_output=True), save_on_close=True, save_on_reset=True, save_dir=replay_save_dir
-    )
-
-    for i in range(games_to_play):
-        # Reset the environment for each game
-        obs, info = env.reset(seed=seed+i)  # changing seed each game
-        env_cfg = info["params"]  # game parameters that agents can see
-
-        player_0 = agent_1_cls("player_0", env_cfg)
-        player_1 = agent_2_cls("player_1", env_cfg)
-
-        game_done = False
-        step = 0
-        print(f"Running game {i+1}/{games_to_play}")
-        while not game_done:
-            actions = {
-                "player_0": player_0.act(step=step, obs=obs["player_0"]),
-                "player_1": player_1.act(step=step, obs=obs["player_1"]),
-            }
-
-            obs, reward, terminated, truncated, info = env.step(actions)
-            dones = {k: terminated[k] or truncated[k] for k in terminated}
-            if dones["player_0"] or dones["player_1"]:
-                game_done = True
-            step += 1
-
-    env.close()  # saves the replay of the last game and frees resources
-    print(f"Finished {games_to_play} games. Replays saved to {replay_save_dir}")
+def agent_fn(observation, configurations):
+    """
+    agent definition for kaggle submission.
+    """
+    global agent_dict
+    obs = observation.obs
+    if type(obs) == str:
+        obs = json.loads(obs)
+    step = observation.step
+    player = observation.player
+    remainingOverageTime = observation.remainingOverageTime
+    if step == 0:
+        agent_dict[player] = RelicHuntingShootingAgent(player, configurations["env_cfg"])
+    agent = agent_dict[player]
+    actions = agent.act(step, from_json(obs), remainingOverageTime)
+    return dict(action=actions.tolist())
 
 if __name__ == "__main__":
-    # Run evaluation with the dummy Agent against itself
-    evaluate_agents(BaselineAgent, RelicHuntingShootingAgent, games_to_play=6,
-                    replay_save_dir="replays/" + BaselineAgent.__name__ + "_" + RelicHuntingShootingAgent.__name__)
 
-    # After running, you can check the "replays" directory for saved replay files.
-    # You can set breakpoints anywhere in this file or inside the Agent class.
+    def read_input():
+        """
+        Reads input from stdin
+        """
+        try:
+            return input()
+        except EOFError as eof:
+            raise SystemExit(eof)
+    step = 0
+    player_id = 0
+    env_cfg = None
+    i = 0
+    while True:
+        inputs = read_input()
+        raw_input = json.loads(inputs)
+        observation = Namespace(**dict(step=raw_input["step"], obs=raw_input["obs"], remainingOverageTime=raw_input["remainingOverageTime"], player=raw_input["player"], info=raw_input["info"]))
+        if i == 0:
+            env_cfg = raw_input["info"]["env_cfg"]
+            player_id = raw_input["player"]
+        i += 1
+        actions = agent_fn(observation, dict(env_cfg=env_cfg))
+        # send actions to engine
+        print(json.dumps(actions))
