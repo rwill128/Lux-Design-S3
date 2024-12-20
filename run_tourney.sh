8d23d2ae444e0455aa226400532e687f71668ca6
Richard Williams <rwill128.create@gmail.com>
2024-12-20 13:10:16 -0300

    Refactors

--- run_tourney.sh
+++ run_tourney.sh
@@ -9,9 +9,6 @@ mkdir -p "$OUTPUT_DIR"
 
 # Run the Lux AI tournament and save the replay
 luxai-s3 agents/baseline_agent/main.py \
-         agents/shooting_vision_agent/main.py \
-         agents/better_shooting_vision_agent/main.py \
-         agents/relic_hunting_shooting_vision_agent/main.py \
          agents/best_agent/main.py \
           --tournament --output "$OUTPUT_DIR/replay.json" --replay.no-compressed-obs --verbose 3 --tournament-cfg-max-episodes 1000 --tournament-cfg-concurrent 15
 
