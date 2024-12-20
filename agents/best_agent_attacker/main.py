c94aa018a38caba47dc3a4ce62cd1047f10c66fb
Richard Williams <rwill128.create@gmail.com>
2024-12-20 16:25:22 -0300

    Everything better?

--- agents/best_agent_attacker/main.py
+++ agents/best_agent_attacker/main.py
@@ -1,10 +1,9 @@
-import os
-from luxai_s3.wrappers import LuxAIS3GymEnv, RecordEpisode
+import json
+from argparse import Namespace
 
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
-from agents.best_agent_attacker.main import BestAgentBetterShooter
 
 class BestAgentAttacker:
     def __init__(self, player: str, env_cfg) -> None:
@@ -322,7 +321,7 @@ class BestAgentAttacker:
         else:
             # This is where I'd like to attack instead.
             self.send_to_attack_if_not_going_to_relic(NON_REWARD_PENALTY, REWARD_BONUS, actions, map_height, map_width,
-                                                       obs, remaining_units, unit_positions)
+                                                      obs, remaining_units, unit_positions)
 
 
         # If the match ended, print known relic positions and reward tiles
@@ -752,49 +751,59 @@ class BestAgentAttacker:
             if (rx, ry) not in self.known_relic_positions:
                 self.known_relic_positions.append((rx, ry))
 
+def from_json(state):
+    if isinstance(state, list):
+        return np.array(state)
+    elif isinstance(state, dict):
+        out = {}
+        for k in state:
+            out[k] = from_json(state[k])
+        return out
+    else:
+        return state
 
-def evaluate_agents(agent_1_cls, agent_2_cls, seed=45, games_to_play=3, replay_save_dir="replays"):
-    # Ensure the replay directory exists
-    os.makedirs(replay_save_dir, exist_ok=True)
+### DO NOT REMOVE THE FOLLOWING CODE ###
+agent_dict = dict() # store potentially multiple dictionaries as kaggle imports code directly
 
-    # Create an environment wrapped to record episodes
-    gym_env = LuxAIS3GymEnv(numpy_output=True)
-    gym_env.render_mode = "human"
-    env = RecordEpisode(
-        gym_env, save_on_close=True, save_on_reset=True, save_dir=replay_save_dir
-    )
-
-
-    for i in range(games_to_play):
-        # Reset the environment for each game
-        obs, info = env.reset(seed=seed+i)  # changing seed each game
-        env_cfg = info["params"]  # game parameters that agents can see
-
-        player_0 = agent_1_cls("player_0", env_cfg)
-        player_1 = agent_2_cls("player_1", env_cfg)
-
-        game_done = False
-        step = 0
-        print(f"Running game {i+1}/{games_to_play}")
-        while not game_done:
-            actions = {
-                "player_0": player_0.act(step=step, obs=obs["player_0"]),
-                "player_1": player_1.act(step=step, obs=obs["player_1"]),
-            }
-
-            obs, reward, terminated, truncated, info = env.step(actions)
-            dones = {k: terminated[k] or truncated[k] for k in terminated}
-            if dones["player_0"] or dones["player_1"]:
-                game_done = True
-            step += 1
-
-    env.close()  # saves the replay of the last game and frees resources
-    print(f"Finished {games_to_play} games. Replays saved to {replay_save_dir}")
+def agent_fn(observation, configurations):
+    """
+    agent definition for kaggle submission.
+    """
+    global agent_dict
+    obs = observation.obs
+    if type(obs) == str:
+        obs = json.loads(obs)
+    step = observation.step
+    player = observation.player
+    remainingOverageTime = observation.remainingOverageTime
+    if step == 0:
+        agent_dict[player] = BestAgentAttacker(player, configurations["env_cfg"])
+    agent = agent_dict[player]
+    actions = agent.act(step, from_json(obs), remainingOverageTime)
+    return dict(action=actions.tolist())
 
 if __name__ == "__main__":
-    # Run evaluation with the dummy Agent against itself
-    evaluate_agents(BestAgentBetterShooter, BestAgentAttacker, games_to_play=20, seed=2,
-                    replay_save_dir="replays/" + BestAgentBetterShooter.__name__ + "_" + BestAgentAttacker.__name__)
 
-    # After running, you can check the "replays" directory for saved replay files.
-    # You can set breakpoints anywhere in this file or inside the Agent class.
+    def read_input():
+        """
+        Reads input from stdin
+        """
+        try:
+            return input()
+        except EOFError as eof:
+            raise SystemExit(eof)
+    step = 0
+    player_id = 0
+    env_cfg = None
+    i = 0
+    while True:
+        inputs = read_input()
+        raw_input = json.loads(inputs)
+        observation = Namespace(**dict(step=raw_input["step"], obs=raw_input["obs"], remainingOverageTime=raw_input["remainingOverageTime"], player=raw_input["player"], info=raw_input["info"]))
+        if i == 0:
+            env_cfg = raw_input["info"]["env_cfg"]
+            player_id = raw_input["player"]
+        i += 1
+        actions = agent_fn(observation, dict(env_cfg=env_cfg))
+        # send actions to engine
+        print(json.dumps(actions))
--- run_tourney.sh
+++ run_tourney.sh
@@ -9,7 +9,7 @@ mkdir -p "$OUTPUT_DIR"
 
 # Run the Lux AI tournament and save the replay
 luxai-s3 agents/baseline_agent/main.py \
-         agents/best_agent/main.py \
+         agents/best_agent_attacker/main.py \
           --tournament --output "$OUTPUT_DIR/replay.json" --replay.no-compressed-obs --verbose 3 --tournament-cfg-max-episodes 5 --tournament-cfg-concurrent 1
 
 echo "Replay saved to: $OUTPUT_DIR/replay.json"
