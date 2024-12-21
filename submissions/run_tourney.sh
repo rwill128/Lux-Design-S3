62daee261128c9b02fab4e65b137a11fe7855226
Richard Williams <rwill128.create@gmail.com>
2024-12-21 20:10:00 -0300

    Adding more versions. 3 and 4 with confidence levels are doing surprisingly well..

--- submissions/run_tourney.sh
+++ submissions/run_tourney.sh
@@ -8,20 +8,19 @@ OUTPUT_DIR="replays/run_$TIMESTAMP"
 mkdir -p "$OUTPUT_DIR"
 
 # Run the Lux AI tournament and save the replay
-luxai-s3 best_agent_attacker_two.py \
-         best_agent_attacker_different_deduce.py \
-         best_agent_attacker_three.py \
-         best_agent_attacker_4.py \
-         best_agent_attacker_five.py \
-         best_agent_attacker_no_nebula.py \
-         best_agent_attacker_no_nebula_two.py \
-         best_agent_attacker_no_nebula_different_explore_no_attack.py \
-         best_agent.py \
+luxai-s3 attacker_two.py \
+         attacker_different_deduce.py \
+         attacker_three.py \
+         attacker_4.py \
+         attacker_five.py \
+         attacker_no_nebula.py \
+         attacker_no_nebula_two.py \
+         attacker_no_nebula_different_explore_no_attack.py \
+         best.py \
          best_agent_no_nebula.py \
          best_agent_better_shooter.py \
          newest_agent/main.py \
          best_agent/main.py \
-         best_agent_attacker/main.py \
         --tournament --output "$OUTPUT_DIR/replay.json" --tournament-cfg-max-episodes 5000 --tournament-cfg-concurrent 15
 
 echo "Replay saved to: $OUTPUT_DIR/replay.json"
